e28c1299437e02b31fca4b65ca1c420fff33158b
İzzedin Çelik <izzeddinmupe@gmail.com>
2020-10-04 01:58:26 +0300

    Update AppTest.java

--- src/main/java/com/jenkins/AppTest.java
+++ src/main/java/com/jenkins/AppTest.java
@@ -4,7 +4,7 @@ public class AppTest {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Jenkins Test was Successfully Completed :) ");
+		System.out.println("Jenkins Test was Successfully Completed:) ");
 
 	}
 
